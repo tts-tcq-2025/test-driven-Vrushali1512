119ee206fccce8bed869851f58ca33da2ff3473f
Vrushali1512 <satawalekarvrushali@gmail.com>
2025-09-22 15:35:16 +0530

    Create String_Calculator.cpp

---
+++ String_Calculator.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// Function to add numbers from a string
+int add(const char* numbers) {
+    // Case 1: If the input is an empty string, return 0
+    if (numbers[0] == '\0') {
+        return 0;
+    }
+
+    // Placeholder: Future logic will go here
+    return 0; // Default value to pass the first test
+}
