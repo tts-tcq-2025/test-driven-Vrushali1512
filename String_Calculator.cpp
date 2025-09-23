ccd0dfb3aaf603224fd7628b94c79ba0699aa1fc
Vrushali1512 <satawalekarvrushali@gmail.com>
2025-09-23 09:15:48 +0530

    Update String_Calculator.cpp

--- String_Calculator.cpp
+++ String_Calculator.cpp
@@ -8,8 +8,13 @@ int add(const char *numbers) {
     return 0;  // Default case (we'll extend this logic later)
 }
 
+void test_single_number() {
+    assert(add("1") == 1);  // The result should be 1
+}
+
 int main() {
     test_empty_string();
-    printf("Test for empty string passed!\n");
+    test_single_number();
+    printf("Test for single number passed!\n");
     return 0;
 }
