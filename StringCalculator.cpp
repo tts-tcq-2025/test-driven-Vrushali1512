ca489b2dfa906585046a32f17b9579ef1c1fe1a6
Vrushali1512 <satawalekarvrushali@gmail.com>
2025-09-30 14:55:37 +0530

    Update StringCalculator.cpp

--- StringCalculator.cpp
+++ StringCalculator.cpp
@@ -7,13 +7,33 @@ int Add(const char *numbers) {
         return 0;
     }
 
-    int sum = 0;
-    char *copy = strdup(numbers);
-    char *token = strtok(copy, ",\n");
+    const char *delimiters = ",\n"; // Default delimiters
+    const char *number_section = numbers;
 
-    while (token) {
+    // Check for custom delimiter syntax "//<delimiter>\n"
+    if (strncmp(numbers, "//", 2) == 0) {
+        char custom_delim = numbers[2];
+        static char custom_delims[2] = {0}; // one char + null terminator
+        custom_delims[0] = custom_delim;
+        custom_delims[1] = '\0';
+        delimiters = custom_delims;
+
+        // Move pointer to after newline
+        number_section = strchr(numbers, '\n');
+        if (number_section != NULL) {
+            number_section++;  // after '\n'
+        } else {
+            return 0;  // invalid format
+        }
+    }
+
+    int sum = 0;
+    char *copy = strdup(number_section);
+    char *token = strtok(copy, delimiters);
+
+    while (token != NULL) {
         sum += atoi(token);
-        token = strtok(NULL, ",\n");
+        token = strtok(NULL, delimiters);
     }
 
     free(copy);
