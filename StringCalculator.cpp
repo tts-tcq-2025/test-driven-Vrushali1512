45503dc549907a8e2468138505bc3c71de82d4d6
Vrushali1512 <satawalekarvrushali@gmail.com>
2025-09-30 14:36:41 +0530

    Update StringCalculator.cpp

--- StringCalculator.cpp
+++ StringCalculator.cpp
@@ -3,13 +3,13 @@
 #include <stdlib.h>
 
 int Add(const char *numbers) {
-    if (numbers == NULL || strlen(numbers) == 0) {
+    if (numbers == NULL || *numbers == '\0') {
         return 0;
     }
 
     int sum = 0;
     char *copy = strdup(numbers);
-    char *token = strtok(copy, ",");
+    char *token = strtok(copy, ",\n");
 
     while (token) {
         sum += atoi(token);
